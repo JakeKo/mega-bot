921738c2701f228839433f9835bcbb2118cb0762
Jake Koperski <wijakoperski@gmail.com>
2020-05-24 19:03:04 -0500

    Overhauled environment-sensitive configuration

--- store.js
+++ store.js
@@ -1,32 +1,7 @@
 const config = require('./config');
 const { MongoClient } = require('mongodb');
 
-const devStore = async () => {
-    const pastaCollection = [
-        {
-            key: 'hello',
-            value: 'Hello World!'
-        }
-    ];
-
-    return {
-        getPastas: async () => {
-            return pastaCollection;
-        },
-        getPasta: async key => {
-            return pastaCollection.find(document => document.key === key);
-        },
-        addPasta: async (key, value) => {
-            pastaCollection.push({ key, value });
-        },
-        removePasta: async key => {
-            const index = pastaCollection.findIndex(document => document.key === key);
-            pastaCollection = pastaCollection.splice(index, 1);
-        }
-    };
-}
-
-const prodStore = async () => {
+module.exports = async () => {
     // Initialize MongoDB connection
     const client = new MongoClient(config.MONGODB_CONNECTION_STRING, {
         useNewUrlParser: true,
@@ -52,5 +27,3 @@ const prodStore = async () => {
         }
     };
 };
-
-module.exports = config.STAGE === 'dev' ? devStore : prodStore;
