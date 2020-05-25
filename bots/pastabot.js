cb32f3e874518a3186b8d887575c5b2dd08bebce
Jake Koperski <wijakoperski@gmail.com>
2020-05-25 01:54:10 -0500

    Simplified regex and improved comments

--- bots/pastabot.js
+++ bots/pastabot.js
@@ -1,10 +1,10 @@
 module.exports = (_, store) => async message => {
     const keys = (await store.getPastas()).map(pasta => pasta.key);
-    const pastaAdd = /^!pasta +add +([0-9a-zA-Z\-_]+) *(.*)/;
+    const pastaAdd = /^!pasta +add +(\S+) *(.*)/;
     const pastaHelp = /^!pasta +help/;
-    const pastaRemove = /^!pasta +remove +([0-9a-zA-Z\-_]+)/;
+    const pastaRemove = /^!pasta +remove +(\S+)/;
     const pastaList = /^!pasta +list/;
-    const pastaSearch = /^!pasta +([0-9a-zA-Z\-_]+)([0-9a-zA-Z\-_\| ]*)/;
+    const pastaSearch = /^!pasta +(\S+)([\S\s]*)/;
     const keywords = ['add', 'help', 'remove', 'list'];
 
     // Check if the message matches '!pasta add [key] [value]'
@@ -75,15 +75,30 @@ module.exports = (_, store) => async message => {
 
         // Display the pasta corresponding to the provided key if one exists
         if (keys.includes(key)) {
+            // Regular expressions are very confusing and take a lot of explanation
+            // I do not have faith in my ability to describe what's going on here, so I hope a few examples do the trick
+            // The provided examples are 'ideal' situations but assume that whitespace is not a problem
             const pasta = await store.getPasta(key);
 
-            // I am proud and scared of what I have done here
+            // Given a pasta value of 'Hello |user1| and |  user2  |!'
+            // 1. 'Hello |user1| and |user2|!' => [['|user1|', 'user1'], ['|user2|'. 'user2']]
+            // 2. [['|user1|', 'user1'], ['|user2|'. 'user2']] => [ 'user1', 'user2' ]
             const expectedArgMatches = [...pasta.value.matchAll(/\| *(\S+) *\|/g)];
-            const providedArgMatches = [...argsString.matchAll(/\| *(\S+)([^\|]+)/g)];
             const expectedArgs = expectedArgMatches.map(match => match[1]);
+
+            // Given a pasta call of '!pasta arg-test |user1 Thing 1 |user2 Thing 2'
+            // 1. '!pasta arg-test |user1 Thing 1 |user2 Thing 2' => [['|user1 Thing 1 ', 'user1', ' Thing 1 '], ['|user2 Thing 2', 'user2', ' Thing 2']]
+            // 2. [['|user1 Thing 1 ', 'user1', ' Thing 1 '], ['|user2 Thing 2', 'user2', ' Thing 2']] => { user1: 'Thing 1', user2: 'Thing 2' }
+            const providedArgMatches = [...argsString.matchAll(/\| *(\S+)([^\|]+)/g)];
             const providedArgs = providedArgMatches.reduce((params, match) => ({ ...params, [match[1]]: match[2].trim() }), {});
 
-            const value = expectedArgs.reduce((output, arg) => output.replace(new RegExp(`\\| *${arg} *\\|`, 'g'), providedArgs[arg]), pasta.value);
+            // For each expected arg, replace all instances of '|arg|' with the provided arg value
+            const value = expectedArgs.reduce((output, arg) => {
+                const regex = new RegExp(`\\| *${arg} *\\|`, 'g');
+                output.replace(regex, providedArgs[arg]);
+            }, pasta.value);
+
+            // Send the fully interpolated message
             message.channel.send(value, { files: pasta.attachments || [] });
         } else {
             message.channel.send(`Could not find a pasta matching the key: **${key}**. Try typing \`!pasta list\` to see the available keys or \`!pasta add\` to add a new pasta.`);
