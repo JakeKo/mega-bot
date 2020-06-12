25de1ba15e2c0e0b135196b9d1e0e53b77494c81
Jake Koperski <wijakoperski@gmail.com>
2020-06-12 16:09:17 -0500

    Added modelling for supportive users

--- src/bots/statbot.js
+++ src/bots/statbot.js
@@ -5,13 +5,14 @@ module.exports = (bot, store) => async message => {
     const statsStatus = /^!stats +status/;
     const statsReacts = /^!stats +reacts/;
     const statsPopular = /^!stats +popular/;
+    const statsSupportive = /^!stats +supportive/;
 
     // Check if the message matches '!stats help'
     if (statsHelp.test(message.content)) {
         message.channel.send([
             '**Usage Intstructions for Stat Bot:**',
             '>>> `!stats help`: View usage instructions for Stat Bot.',
-            '`!stats reacts`: View a list of the most popular reacts in Mega Chat.',
+            '`!stats reacts`: View a list of the most popular reacts in Mega Chat. Filter by the optional user.',
             '`!stats popular`: View the most popular members (based on reacts) in Mega Chat.',
             '`!stats status`: View clerical information about Stat Bot.'
         ].join('\n'));
@@ -33,29 +34,48 @@ module.exports = (bot, store) => async message => {
     // Check if the message matches '!stats reacts'
     else if (statsReacts.test(message.content)) {
         const messages = await store.getMessages();
-        const popularity = modelReactPopularity(messages).slice(0, 10);
+        const popularity = modelReactPopularityData(messages).slice(0, 10);
 
         message.channel.send([
             `**Top ${popularity.length} Most Popular Reacts in Mega Chat:**`,
             '',
-            ...plotReactPopularity(popularity)
+            ...plotReactPopularityData(popularity)
         ].join('\n'));
     }
 
     // Check if the message matches '!stats popular'
     else if (statsPopular.test(message.content)) {
         const messages = await store.getMessages();
-        const popularity = (await modelUserPopularity(messages, bot)).slice(0, 10);
+        const popularity = (await modelUserPopularityData(messages, bot)).slice(0, 10);
 
         message.channel.send([
             `**Top ${popularity.length} Most Popular Users in Mega Chat (by React Count):**`,
             '',
-            ...plotUserPopularity(popularity)
+            ...plotUserPopularityData(popularity)
         ].join('\n'));
+    }
+
+    // Check if the messages matches '!stats supportive'
+    else if (statsSupportive.test(message.content)) {
+        const messages = await store.getMessages();
+        const supportiveData = modelSupportiveData(messages);
+        console.log(supportiveData);
     }
 };
 
-function modelReactPopularity(messages) {
+async function getDisplayName(key, bot) {
+    let displayName = '';
+
+    try {
+        displayName = (await bot.guilds.cache.first().members.fetch(key)).displayName;
+    } catch {
+        console.log(`Bad User ID: ${key}`);
+    }
+
+    return displayName;
+}
+
+function modelReactPopularityData(messages) {
     const popularity = {};
 
     messages.forEach(({ reacts }) => {
@@ -69,41 +89,48 @@ function modelReactPopularity(messages) {
         .sort((a, b) => a.count >= b.count ? -1 : 1);
 }
 
-function plotReactPopularity(reacts) {
+function plotReactPopularityData(reacts) {
     const maxValue = Math.max(...reacts.map(react => react.count));
     const maxWidth = 20;
 
     return reacts.map(({ reactId, count }) => `${reactId} ${Array(Math.round(maxWidth * count / maxValue) + 1).join('█')} ${count}`);
 }
 
-async function modelUserPopularity(messages, bot) {
+async function modelUserPopularityData(messages, bot) {
     const popularity = {};
 
     messages.forEach(({ author, reacts }) => {
         popularity[author] = popularity[author] === undefined ? reacts.length : popularity[author] + reacts.length;
     });
 
-    // Map user ID to display name
-    const members = bot.guilds.cache.first().members;
     return (await Promise.all(Object.keys(popularity)
         .filter(key => !config.STATBOT_ID_BLACKLIST.includes(key))
-        .map(async key => {
-            let userId = '';
-
-            try {
-                userId = (await members.fetch(key)).displayName;
-            } catch {
-                console.log(`Bad User ID: ${key}`);
-            }
-
-            return { userId, reactCount: popularity[key] };
-        }))).sort((a, b) => a.reactCount >= b.reactCount ? -1 : 1);
+        .map(async key => ({
+            userId: await getDisplayName(key, bot),
+            reactCount: popularity[key]
+        })))).sort((a, b) => a.reactCount >= b.reactCount ? -1 : 1);
 }
 
-function plotUserPopularity(users) {
+function plotUserPopularityData(users) {
     const maxLabelLength = Math.max(...users.map(user => user.userId.length));
     const maxValue = Math.max(...users.map(user => user.reactCount));
     const maxWidth = 15;
 
     return users.map(({ userId, reactCount }) => `\`${userId.padStart(maxLabelLength, ' ')}\`: ${Array(Math.round(maxWidth * reactCount / maxValue) + 1).join('█')} ${reactCount}`);
+}
+
+function modelSupportiveData(messages) {
+    const supports = {};
+    
+    messages.forEach(({ author, reacts }) => {
+        const support = supports[author] === undefined ? {} : { ...supports[author] };
+        
+        reacts.forEach(({ user }) => {
+            support[user] = support[user] === undefined ? 1 : support[user] + 1;
+        });
+
+        supports[author] = support;
+    });
+
+    return supports;
 }
