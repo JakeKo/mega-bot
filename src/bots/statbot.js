cc6679889ad957e20c0259b0d908a5ef05a8ad48
Jake Koperski <wijakoperski@gmail.com>
2022-02-22 09:40:27 -0600

    Returned Stat Bot to its former glory

--- src/bots/statbot.js
+++ src/bots/statbot.js
@@ -1,13 +1,14 @@
-const { getDisplayNames, getDisplayName, getAllDisplayNames } = require('../utilities');
+const { getDisplayName, flattenEntries, barPlot, evaluateUserQuery } = require('../utilities');
 const Logger = require('../logger');
+const config = require('../../config');
 
 module.exports = (bot, store) => async message => {
     const stats = /^!stats/;
     const statsHelp = /^!stats +help/;
     const statsStatus = /^!stats +status/;
     const statsReacts = /^!stats +reacts(.*)/;
-    // const statsPopular = /^!stats +popular(.*)/;
-    // const statsSupportive = /^!stats +supportive(.*)/;
+    const statsPopular = /^!stats +popular(.*)/;
+    const statsSupportive = /^!stats +supportive(.*)/;
 
     if (!stats.test(message.content)) {
         return;
@@ -65,106 +66,76 @@ module.exports = (bot, store) => async message => {
     }
 
     // Check if the message matches '!stats popular [?user]'
-    // else if (statsPopular.test(message.content)) {
-    //     const [, userQuery] = message.content.match(statsPopular);
-    //     const messages = await store.getMessages();
+    else if (statsPopular.test(message.content)) {
+        const [, userQuery] = message.content.match(statsPopular);
+        const messages = await store.getMessages();
 
-    //     // Check if no user was provided
-    //     if (userQuery.trim() === '') {
-    //         const data = modelGlobalUserPopularityData(messages).slice(0, 10);
-    //         data.length === 0
-    //             ? message.channel.send('No data available for the provided command.')
-    //             : message.channel.send(plotGlobalUserPopularityData(data));
-    //     } else {
-    //         const result = await evaluateUserQuery(bot, userQuery);
+        // Check if no user was provided
+        if (userQuery.trim() === '') {
+            const data = modelGlobalUserPopularityData(bot, messages).slice(0, 10);
+            const response = data.length === 0
+                ? 'No data available for the provided command.'
+                : await plotGlobalUserPopularityData(bot, data);
+            
+            message.channel.send(response);
+        } else {
+            const result = await evaluateUserQuery(bot, userQuery);
 
-    //         if (result.success) {
-    //             const data = await modelIndividualUserPopularityData(messages, result.user).slice(0, 10);
-    //             const displayName = await getDisplayName(bot, result.user);
-    //             data.length === 0
-    //                 ? message.channel.send('No data available for the provided command.')
-    //                 : message.channel.send(plotIndividualUserPopularityData(data, displayName));
-    //         } else {
-    //             message.channel.send(result.message);
-    //         }
-    //     }
-    // }
+            if (result.success) {
+                const data = modelIndividualUserPopularityData(messages, result.user).slice(0, 10);
+                const displayName = await getDisplayName(bot, result.user);
+                const response = data.length === 0
+                    ? 'No data available for the provided command.'
+                    : await plotIndividualUserPopularityData(bot, data, displayName);
+                
+                message.channel.send(response);
+            } else {
+                message.channel.send(result.message);
+            }
+        }
+    }
 
     // Check if the messages matches '!stats supportive [?user]'
-    // else if (statsSupportive.test(message.content)) {
-    //     const [, userQuery] = message.content.match(statsSupportive);
-    //     const messages = await store.getMessages();
+    else if (statsSupportive.test(message.content)) {
+        const [, userQuery] = message.content.match(statsSupportive);
+        const messages = await store.getMessages();
 
-    //     // Check if no user was provided
-    //     if (userQuery.trim() === '') {
-    //         const data = modelGlobalSupportiveData(messages).slice(0, 10);
-    //         data.length === 0
-    //             ? message.channel.send('No data available for the provided command.')
-    //             : message.channel.send(plotGlobalSupportiveData(data));
-    //     } else {
-    //         const result = await evaluateUserQuery(bot, userQuery);
+        // Check if no user was provided
+        if (userQuery.trim() === '') {
+            const data = modelGlobalSupportiveData(messages).slice(0, 10);
+            const response = data.length === 0
+                ? 'No data available for the provided command.'
+                : await plotGlobalSupportiveData(bot, data);
+            
+            message.channel.send(response);
+        } else {
+            const result = await evaluateUserQuery(bot, userQuery);
 
-    //         if (result.success) {
-    //             const data = modelIndividualSupportiveData(messages, result.user).slice(0, 10);
-    //             const displayName = await getDisplayName(bot, result.user);
-    //             data.length === 0
-    //                 ? message.channel.send('No data available for the provided command.')
-    //                 : message.channel.send(plotIndividualSupportiveData(data, displayName));
-    //         } else {
-    //             message.channel.send(result.message);
-    //         }
-    //     }
-    // }
-};
-
-// Returns the IDs of users whose display name matches the query
-async function searchUsers(bot, query) {
-    const allNames = await getAllDisplayNames(bot);
-    return Object.keys(allNames).filter(id => allNames[id].toLowerCase().includes(query.toLowerCase()));
-}
-
-async function evaluateUserQuery(bot, userQuery) {
-    const query = userQuery.trim();
-    const targetUsers = await searchUsers(bot, query);
-
-    // Check how many users matched the provided user query
-    if (targetUsers.length === 1) {
-        return { success: true, user: targetUsers[0], message: 'Query Successful' };
-    } else if (targetUsers.length === 0) {
-        return { success: false, user: '', message: `Query Error: No user matches query "${query}".` };
-    } else {
-        const displayNames = await getDisplayNames(bot, ...targetUsers);
-        const displayNamesString = Object.values(displayNames).join('\n');
-        return { success: false, user: '', message: `Query Error: More than one user matches query "${query}": ${displayNamesString}` };
+            if (result.success) {
+                const data = modelIndividualSupportiveData(messages, result.user).slice(0, 10);
+                const displayName = await getDisplayName(bot, result.user);
+                const response = data.length === 0
+                    ? 'No data available for the provided command.'
+                    : await plotIndividualSupportiveData(bot, data, displayName);
+                
+                message.channel.send(response);
+            } else {
+                message.channel.send(result.message);
+            }
+        }
     }
-}
+};
 
 // INDIVIDUAL REACT POPULARITY DATA
 function modelIndividualReactPopularityData(messages, user) {
     const popularity = {};
-
     messages.filter(m => m.author === user).forEach(({ reacts }) => {
         reacts.forEach(({ react }) => {
             popularity[react] = 1 + (popularity[react] || 0);
         });
     });
 
-    return Object.keys(popularity)
-        .map(id => ({ reactId: id, count: popularity[id] }))
-        .sort((a, b) => a.count >= b.count ? -1 : 1);
-}
-
-function plotIndividualReactPopularityData(data, user) {
-    const maxValue = Math.max(...data.map(react => react.count));
-    const maxWidth = 20;
-
-    return [
-        `**Most Popular Reacts by ${user}:**`,
-        ...data.map(({ reactId, count }) => {
-            const bar = Array(Math.round(maxWidth * count / maxValue) + 1).join('█');
-            return `${reactId}\t${bar} ${count}`;
-        })
-    ].join('\n');
+    return Object.entries(popularity).sort((a, b) => a[1] >= b[1] ? -1 : 1);
 }
 
 // GLOBAL REACT POPULARITY DATA
@@ -176,142 +147,126 @@ function modelGlobalReactPopularityData(messages) {
         });
     });
 
-    return Object.keys(popularity)
-        .map(id => ({ reactId: id, count: popularity[id] }))
-        .sort((a, b) => a.count >= b.count ? -1 : 1);
-}
-
-function plotGlobalReactPopularityData(data) {
-    const maxValue = Math.max(...data.map(react => react.count));
-    const maxWidth = 20;
-
-    return [
-        '**Most Popular Reacts:**',
-        ...data.map(({ reactId, count }) => {
-            const bar = Array(Math.round(maxWidth * count / maxValue) + 1).join('█');
-            return `${reactId}\t${bar} ${count}`;
-        })
-    ].join('\n');
+    return Object.entries(popularity).sort((a, b) => a[1] >= b[1] ? -1 : 1);
 }
 
 // INDIVIDUAL USER POPULARITY DATA
-// async function modelIndividualUserPopularityData(bot, messages, user) {
-//     const popularityMap = await Promise.all(messages.map(async m => {
-//         const { author, reacts } = m;
-//         const displayName = await getDisplayName(bot, author);
-//         const reactCount = reacts.filter(r => r.user === user).length;
-//         return [displayName, reactCount];
-//     }));
+function modelIndividualUserPopularityData(bot, messages, user) {
+    const entries = messages.map(({ author, reacts }) => {
+        const reactCount = reacts.filter(r => r.user === user).length;
+        return [author, reactCount];
+    });
 
-//     const popularityObject = {};
-//     popularityMap.forEach(([name, count]) => {
-//         popularityObject[name] = count + (popularityMap[name] || 0);
-//     });
-
-//     return Object.keys(popularityObject)
-//         .map(id => ({ userId: id, reactCount: popularityObject[id] }))
-//         .sort((a, b) => a.reactCount >= b.reactCount ? -1 : 1);
-// }
-
-// function plotIndividualUserPopularityData(data, user) {
-//     const maxLabelLength = Math.max(...data.map(user => user.userId.length));
-//     const maxValue = Math.max(...data.map(user => user.reactCount));
-//     const maxWidth = 15;
-
-//     return [
-//         `**Most Popular Users According to ${user}:**`,
-//         ...data.map(({ userId, reactCount }) => {
-//             const label = userId.padStart(maxLabelLength, ' ');
-//             const bar = Array(Math.round(maxWidth * reactCount / maxValue) + 1).join('█');
-//             return `\`${label}\`: ${bar} ${reactCount}`;
-//         })
-//     ].join('\n');
-// }
+    const flatEntries = Object.entries(flattenEntries(entries));
+    return flatEntries.sort((a, b) => a[1] >= b[1] ? -1 : 1);
+}
 
 // GLOBAL USER POPULARITY DATA
-// function modelGlobalUserPopularityData(messages) {
-//     const popularity = {};
-//     messages.forEach(({ author, reacts }) => {
-//         const displayName = getDisplayNames()[author] || 'NO NAME';
-//         popularity[displayName] = reacts.length + (popularity[displayName] || 0);
-//     });
+function modelGlobalUserPopularityData(bot, messages) {
+    const entries = messages
+        .filter(m => !config.STATBOT_ID_BLACKLIST.includes(m.author))
+        .map(({ author, reacts }) => [author, reacts.length]);
 
-//     return Object.keys(popularity)
-//         .map(id => ({ userId: id, reactCount: popularity[id] }))
-//         .sort((a, b) => a.reactCount >= b.reactCount ? -1 : 1);
-// }
-
-// function plotGlobalUserPopularityData(data) {
-//     const maxLabelLength = Math.max(...data.map(user => user.userId.length));
-//     const maxValue = Math.max(...data.map(user => user.reactCount));
-//     const maxWidth = 15;
-
-//     return [
-//         '**Most Popular Users:**',
-//         ...data.map(({ userId, reactCount }) => {
-//             const label = userId.padStart(maxLabelLength, ' ');
-//             const bar = Array(Math.round(maxWidth * reactCount / maxValue) + 1).join('█');
-//             return `\`${label}\`: ${bar} ${reactCount}`;
-//         })
-//     ].join('\n');
-// }
+    const flatEntries = Object.entries(flattenEntries(entries));
+    return flatEntries.sort((a, b) => a[1] >= b[1] ? -1 : 1);
+}
 
 // INDIVIDUAL SUPPORTIVE DATA
-// function modelIndividualSupportiveData(messages, user) {
-//     const support = {};
-//     messages.filter(m => m.author === user).forEach(({ reacts }) => {
-//         reacts.forEach(({ user }) => {
-//             const displayName = getDisplayNames()[user] || 'NO NAME';
-//             support[displayName] = 1 + (support[displayName] || 0);
-//         });
-//     });
+function modelIndividualSupportiveData(messages, user) {
+    const support = {};
+    messages.filter(m => m.author === user).forEach(({ reacts }) => {
+        reacts.forEach(({ user }) => {
+            support[user] = 1 + (support[user] || 0);
+        });
+    });
 
-//     return Object.keys(support)
-//         .map(id => ({ userId: id, reactCount: support[id] }))
-//         .sort((a, b) => a.reactCount >= b.reactCount ? -1 : 1);
-// }
-
-// function plotIndividualSupportiveData(data, user) {
-//     const maxLabelLength = Math.max(...data.map(user => user.userId.length));
-//     const maxValue = Math.max(...data.map(user => user.reactCount));
-//     const maxWidth = 15;
-
-//     return [
-//         `**Most Supportive Users of ${user}:**`,
-//         ...data.map(({ userId, reactCount }) => {
-//             const label = userId.padStart(maxLabelLength, ' ');
-//             const bar = Array(Math.round(maxWidth * reactCount / maxValue) + 1).join('█');
-//             return `\`${label}\`: ${bar} ${reactCount}`;
-//         })
-//     ].join('\n');
-// }
+    return Object.entries(support).sort((a, b) => a[1] >= b[1] ? -1 : 1);
+}
 
 // GLOBAL SUPPORTIVE DATA
-// function modelGlobalSupportiveData(messages) {
-//     const support = {};
-//     messages.forEach(({ reacts }) => {
-//         reacts.forEach(({ user }) => {
-//             const displayName = getDisplayNames()[user] || 'NO NAME';
-//             support[displayName] = 1 + (support[displayName] || 0);
-//         });
-//     });
+function modelGlobalSupportiveData(messages) {
+    const support = {};
+    messages.forEach(({ reacts }) => {
+        reacts.forEach(({ user }) => {
+            support[user] = 1 + (support[user] || 0);
+        });
+    });
 
-//     return Object.keys(support)
-//         .map(id => ({ userId: id, reactCount: support[id] }))
-//         .sort((a, b) => a.reactCount >= b.reactCount ? -1 : 1);
-// }
+    return Object.entries(support).sort((a, b) => a[1] >= b[1] ? -1 : 1);
+}
 
-// function plotGlobalSupportiveData(data) {
-//     const maxLabelLength = Math.max(...data.map(user => user.userId.length));
-//     const maxValue = Math.max(...data.map(user => user.reactCount));
-//     const maxWidth = 15;
+function plotIndividualReactPopularityData(entries, user) {
+    return barPlot(`**Most Popular Reacts by ${user}:**`, entries);
+}
 
-//     return [
-//         '**Most Supportive Users:**',
-//         ...data.map(({ userId, reactCount }) => {
-//             const label = userId.padStart(maxLabelLength, ' ');
-//             const bar = Array(Math.round(maxWidth * reactCount / maxValue) + 1).join('█');
-//             return `\`${label}\`: ${bar} ${reactCount}`;
-//         })
-//     ].join('\n');
-// }
+function plotGlobalReactPopularityData(entries) {
+    return barPlot('**Most Popular Reacts:**', entries);
+}
+
+async function plotIndividualUserPopularityData(bot, data, user) {
+    const label = `**Most Popular Users According to ${user}:**`;
+    let maxKeyLength = 0;
+    let entries = await Promise.all(data.map(async ([key, value]) => {
+        const displayName = await getDisplayName(bot, key);
+
+        if (displayName.length > maxKeyLength) {
+            maxKeyLength = displayName.length;
+        }
+
+        return [displayName, value];
+    }));
+    entries = entries.map(([key, value]) => [`\`${key.padStart(maxKeyLength, ' ')}\``, value]);
+
+    return barPlot(label, entries);
+}
+
+async function plotGlobalUserPopularityData(bot, data) {
+    const label = '**Most Popular Users:**';
+    let maxKeyLength = 0;
+    let entries = await Promise.all(data.map(async ([key, value]) => {
+        const displayName = await getDisplayName(bot, key);
+
+        if (displayName.length > maxKeyLength) {
+            maxKeyLength = displayName.length;
+        }
+
+        return [displayName, value];
+    }));
+    entries = entries.map(([key, value]) => [`\`${key.padStart(maxKeyLength, ' ')}\``, value]);
+
+    return barPlot(label, entries);
+}
+
+async function plotIndividualSupportiveData(bot, data, user) {
+    const label = `**Most Supportive Users of ${user}:**`;
+    let maxKeyLength = 0;
+    let entries = await Promise.all(data.map(async ([key, value]) => {
+        const displayName = await getDisplayName(bot, key);
+
+        if (displayName.length > maxKeyLength) {
+            maxKeyLength = displayName.length;
+        }
+
+        return [displayName, value];
+    }));
+    entries = entries.map(([key, value]) => [`\`${key.padStart(maxKeyLength, ' ')}\``, value]);
+
+    return barPlot(label, entries);
+}
+
+async function plotGlobalSupportiveData(bot, data) {
+    const label = '**Most Supportive Users:**';
+    let maxKeyLength = 0;
+    let entries = await Promise.all(data.map(async ([key, value]) => {
+        const displayName = await getDisplayName(bot, key);
+
+        if (displayName.length > maxKeyLength) {
+            maxKeyLength = displayName.length;
+        }
+
+        return [displayName, value];
+    }));
+    entries = entries.map(([key, value]) => [`\`${key.padStart(maxKeyLength, ' ')}\``, value]);
+
+    return barPlot(label, entries);
+}
--- src/utilities.js
+++ src/utilities.js
@@ -40,9 +40,61 @@ function getArgs(s) {
     return { args: argObject, body: body.join(' ') };
 }
 
+/**
+ * Converts a list of key-value pairs into an object.
+ * Naively adds values if two or more keys match.
+ */
+function flattenEntries(entries) {
+    const obj = {};
+    entries.forEach(([key, value]) => {
+        if (key in obj) {
+            obj[key] = obj[key] + value;
+        } else {
+            obj[key] = value;
+        }
+    });
+
+    return obj;
+}
+// Returns the IDs of users whose display name matches the query
+async function searchUsers(bot, query) {
+    const allNames = await getAllDisplayNames(bot);
+    return Object.keys(allNames).filter(id => allNames[id].toLowerCase().includes(query.toLowerCase()));
+}
+
+async function evaluateUserQuery(bot, userQuery) {
+    const query = userQuery.trim();
+    const targetUsers = await searchUsers(bot, query);
+
+    // Check how many users matched the provided user query
+    if (targetUsers.length === 1) {
+        return { success: true, user: targetUsers[0], message: 'Query Successful' };
+    } else if (targetUsers.length === 0) {
+        return { success: false, user: '', message: `Query Error: No user matches query "${query}".` };
+    } else {
+        const displayNames = await getDisplayNames(bot, ...targetUsers);
+        const displayNamesString = Object.values(displayNames).map(s => `\`${s}\``).join(', ');
+        return { success: false, user: '', message: `Query Error: More than one user matches query "${query}":\n${displayNamesString}` };
+    }
+}
+
+function barPlot(label, entries, width = 20) {
+    const maxValue = Math.max(...entries.map(e => e[1]));
+    const bars = entries.map(([key, value]) => {
+        const barLength = maxValue === 0 ? -1 : Math.round(width * value / maxValue);
+        const bar = Array(barLength + 1).join('█');
+        return `${key} ${bar} ${value}`;
+    });
+
+    return [label, ...bars].join('\n');
+}
+
 module.exports = {
     getDisplayName,
     getDisplayNames,
     getAllDisplayNames,
-    getArgs
+    getArgs,
+    flattenEntries,
+    evaluateUserQuery,
+    barPlot
 };
